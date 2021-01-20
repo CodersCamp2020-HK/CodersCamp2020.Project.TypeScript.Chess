5fd7f55a52c2b0824de1c0ea395bd7f17917f230
bat098 <mateusz.baciak@gmail.com>
2021-01-20 16:00:30 +0100

    v1.1 scss, css module, ts, images, dist cleaner, html recreate

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
   entry: './src/index.ts',
 
 
-
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
 
 
   module: {
