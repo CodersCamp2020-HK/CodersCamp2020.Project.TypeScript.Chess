fdb5066f63e6f26d1e4a6c576f3f124bc0ae3cba
Haivex <demozial@gmail.com>
2021-01-23 16:14:25 +0100

    Tiles are generated

--- src/app/views/ChessBoardComponent.ts
+++ src/app/views/ChessBoardComponent.ts
@@ -1,6 +1,7 @@
 import { ChessBoardRepresentation } from '../models/EngineInterface';
 import { Piece } from '../models/EngineInterface';
 import { Cord } from '../models/EngineInterface';
+import { Side } from '../models/EngineInterface';
 
 interface PieceFile {
     url: string,
@@ -16,19 +17,22 @@ export class ChessBoardComponent {
     piecesFiles: PieceFile[];
     constructor(blackTilesClassList: string, whiteTilesClassList: string, piecesFiles: PieceFile[], chessBoardRepresentation: ChessBoardRepresentation) {
         this.wrapper = document.createElement('div');
+        this.wrapper.className = 'board-wrapper';
         this.chessBoardElement = document.createElement('div');
         this.verticalAxi = document.createElement('div');
+        this.verticalAxi.className = 'vertical-axi';
         this.horizontalAxi = document.createElement('div');
+        this.horizontalAxi.className = 'horizontal-axi';
         this.chessBoardElement = document.createElement('div');
         this.blackTilesClassList = blackTilesClassList;
-        this.whiteTilesClassList = blackTilesClassList;
+        this.whiteTilesClassList = whiteTilesClassList;
         this.piecesFiles = piecesFiles;
 
         this.wrapper.appendChild(this.verticalAxi);
         this.wrapper.appendChild(this.horizontalAxi);
         this.wrapper.appendChild(this.chessBoardElement);
 
-        //this.render(chessBoardRepresentation);
+        this.render(chessBoardRepresentation);
     }
 
     render(chessBoardRepresentation: ChessBoardRepresentation) {
@@ -42,11 +46,11 @@ export class ChessBoardComponent {
         let currentTile = blackTile;
         this.chessBoardElement.innerHTML = "";
 
-        for(let i = 0; i < chessBoardRepresentation.length-1; i++) {
+        for(let i = 0; i < chessBoardRepresentation.length; i++) {
 
-           currentTile = currentTile == whiteTile ? blackTile : whiteTile;
+            currentTile = currentTile == blackTile ? whiteTile : blackTile;
 
-            for(let j = 0; j < chessBoardRepresentation[i].length-1; j++) {
+            for(let j = 0; j < chessBoardRepresentation[i].length; j++) {
                 let tileToAppend = currentTile.cloneNode(true) as HTMLDivElement;
                 tileToAppend.dataset.x = i.toString();
                 tileToAppend.dataset.y = j.toString();
@@ -56,10 +60,11 @@ export class ChessBoardComponent {
 
                 this.chessBoardElement.appendChild(tileToAppend);
 
-                currentTile = currentTile == whiteTile ? blackTile : whiteTile;
+                currentTile = currentTile == blackTile ? whiteTile : blackTile;
+
             }
 
+            currentTile = currentTile == blackTile ? whiteTile : blackTile;
         }
-        return 
     }
 }
