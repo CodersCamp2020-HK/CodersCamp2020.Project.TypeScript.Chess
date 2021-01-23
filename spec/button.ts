5a4acda886c3980045ce03a206443ca9263fc8c3
Tomasz Dudek <t.dudeek@gmail.com>
2021-01-23 16:58:42 +0100

    generic-button

---
+++ spec/button.ts
@@ -0,0 +1,17 @@
+// Nadaj podstawowo klasę .button.
+// Napisz gettera do elementu.
+
+export class Button {
+  private __button: HTMLButtonElement;
+
+  constructor(tekst: string, classList: string[], callback: any) {
+    this.__button = document.createElement('button');
+    this.__button.classList.add('button', ...classList);
+    this.__button.innerText = tekst;
+    this.__button.addEventListener('click', () => callback());
+  }
+
+  public get button(): HTMLButtonElement {
+    return this.__button;
+  }
+}
