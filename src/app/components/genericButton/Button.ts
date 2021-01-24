36c134899d2ebe88176c587e44f627c630e30dee
Tomasz Dudek <t.dudeek@gmail.com>
2021-01-24 22:16:49 +0100

    finished button

--- src/app/components/genericButton/Button.ts
+++ src/app/components/genericButton/Button.ts
@@ -1,14 +1,16 @@
+import styles from './Button.module.scss';
+
 export class Button {
-  private __button: HTMLButtonElement;
+    private __button: HTMLButtonElement;
 
-  constructor(text: string, classList: string[], onClickCb: any) {
-    this.__button = document.createElement('button');
-    this.__button.classList.add('button', ...classList);
-    this.__button.innerText = text;
-    this.__button.addEventListener('click', () => onClickCb());
-  }
+    constructor(text: string, classList: string[], onClickCb: any) {
+        this.__button = document.createElement('button');
+        this.__button.classList.add(styles.button, ...classList);
+        this.__button.innerText = text;
+        this.__button.addEventListener('click', () => onClickCb());
+    }
 
-  public get button(): HTMLButtonElement {
-    return this.__button;
-  }
+    public get button(): HTMLButtonElement {
+        return this.__button;
+    }
 }
