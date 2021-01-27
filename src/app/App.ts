6c600ee4b08c41dbd6b6e5934750b0d5bdb82fbe
Mateusz Król <matek8952@gmail.com>
2021-01-27 02:01:18 +0100

    Working bishop moves, moves outside chessboard removed

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,5 +1,29 @@
+import { Cord, IChessBoard, PieceType, Side, Piece } from './domain/basicChessTypes';
+import { ChessEngine } from './infrastructure/ChessEngine';
+
 const App = (): void => {
-    console.log('yo');
+    const chessEngine = new ChessEngine();
+    const cord: Cord = { x: 3, y: 4 };
+    const bishop: Piece = {
+        figType: PieceType.Bishop,
+        cord: cord,
+        side: Side.White,
+        isMoved: true,
+    };
+    const board: IChessBoard = {
+        board: [
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, bishop, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+        ],
+    };
+    console.log(board.board);
+    console.log(chessEngine.getPossibleMovesForBishop(cord, board));
 };
 
 export default App;
--- src/app/infrastructure/ChessEngine.ts
+++ src/app/infrastructure/ChessEngine.ts
@@ -1,4 +1,4 @@
-import { PieceType, Cord, IChessBoard, CordWithMoveType, Side } from '../domain/basicChessTypes';
+import { PieceType, Cord, IChessBoard, CordWithMoveType, Side, MoveType } from '../domain/basicChessTypes';
 import { IChessEngine } from '../domain/IChessEngine';
 import _ from 'lodash';
 
@@ -39,14 +39,35 @@ export class ChessEngine implements IChessEngine {
     }
     getPossibleMovesForBishop(cord: Cord, boardState: IChessBoard): CordWithMoveType[] {
         const { x, y } = cord;
-        const aaa = boardState.board[x][y];
-        if (aaa !== null) {
-            const piece = aaa.figType;
+        const board = boardState.board[x][y];
+        const result: CordWithMoveType[] = [];
+        if (board) {
+            const piece = board.figType;
             if (piece !== PieceType.Bishop) throw new Error('Piece is not a Bishop');
+
+            const directions = [
+                ..._.zip(_.range(x + 1, 8, 1), _.range(y + 1, 8, 1)),
+                ..._.zip(_.range(x + 1, 8, 1), _.range(y - 1, -1, -1)),
+                ..._.zip(_.range(x - 1, -1, -1), _.range(y + 1, 8, 1)),
+                ..._.zip(_.range(x - 1, -1, -1), _.range(y - 1, -1, -1)),
+            ];
+            console.log(directions);
+            for (const move of directions) {
+                if (move[0] !== undefined && move[1] !== undefined) {
+                    const propperCord = { x: move[0], y: move[1] } as Cord;
+                    const propperCordMove = {
+                        ...propperCord,
+                        moveType: MoveType.NormalMove,
+                    } as CordWithMoveType;
+                    result.push(propperCordMove);
+                }
+            }
+            return result;
         }
 
         return [];
     }
+
     getPossibleMovesForQueen(cord: Cord, boardState: IChessBoard): CordWithMoveType[] {
         return this.getPossibleMovesForBishop(cord, boardState).concat(this.getPossibleMovesForRook(cord, boardState));
     }
