f6492645b4f5eac27422c58f736ce9d3984dddb7
Haivex <demozial@gmail.com>
2021-01-27 18:45:28 +0100

    Added initial styles

--- src/app/components/ChessBoard/chess.module.scss.d.ts
+++ src/app/components/ChessBoard/chess.module.scss.d.ts
@@ -4,3 +4,7 @@ export const white: string;
 export const board: string;
 export const vertical_axi: string;
 export const horizontal_axi: string;
+export const selected: string;
+export const possible__move: string;
+export const possible__move__hover: string;
+export const capture: string;
