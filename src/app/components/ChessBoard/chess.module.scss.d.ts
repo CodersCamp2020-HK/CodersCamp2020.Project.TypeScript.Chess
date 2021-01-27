fbf04660adc2e22ec8b083fb7bdef091566d4ea6
Haivex <demozial@gmail.com>
2021-01-27 19:26:19 +0100

    Added actions to css

--- src/app/components/ChessBoard/chess.module.scss.d.ts
+++ src/app/components/ChessBoard/chess.module.scss.d.ts
@@ -8,3 +8,6 @@ export const selected: string;
 export const possible__move: string;
 export const possible__move__hover: string;
 export const capture: string;
+export const pasant: string;
+export const castling__right: string;
+export const castling__left: string;
