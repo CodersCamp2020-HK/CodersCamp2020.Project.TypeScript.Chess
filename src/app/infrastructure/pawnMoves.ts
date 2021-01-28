319215365b52fac5290d416a3f65a1eb912e71aa
Haivex <demozial@gmail.com>
2021-01-28 15:29:22 +0100

    Initial commit

---
+++ src/app/infrastructure/pawnMoves.ts
@@ -0,0 +1,10 @@
+describe('Possible Moves for Pawn', () => {
+    describe('Passant', () => {
+        test('one', () => {
+            expect(true).toBeTruthy();
+        });
+        test('two', () => {
+            expect(true).toBeTruthy();
+        });
+    });
+});
