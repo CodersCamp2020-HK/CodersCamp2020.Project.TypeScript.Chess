8ba194074b274cec0c65173e1ac2dc1ec2dd6d44
bat098 <mateusz.baciak@gmail.com>
2021-01-28 20:40:42 +0100

    style fix

--- src/app/components/Aside/Aside.module.scss.d.ts
+++ src/app/components/Aside/Aside.module.scss.d.ts
@@ -1,6 +1,5 @@
-// This file is generated automatically
-export const wrapper: string;
-export const left: string;
 export const active: string;
 export const btn: string;
+export const left: string;
 export const right: string;
+export const wrapper: string;
