cafd164e0fa77c038857b4a49c688901c48a5764
Haivex <demozial@gmail.com>
2021-01-29 15:56:43 +0100

    Written pseudo code

--- src/app/infrastructure/pawnMoves.ts
+++ src/app/infrastructure/pawnMoves.ts
@@ -1,4 +1,11 @@
-import { Cord, IChessBoard, CordWithMoveType, MoveType, ChessBoardRepresentation } from '../domain/basicChessTypes';
+import {
+    Cord,
+    IChessBoard,
+    CordWithMoveType,
+    MoveType,
+    ChessBoardRepresentation,
+    PieceType,
+} from '../domain/basicChessTypes';
 //import { IChessEngine } from '../domain/IChessEngine';
 
 export function getSingleMove(
@@ -23,28 +30,99 @@ export function getDoubleMove(
     moveDirection: number,
     currentBoardState: ChessBoardRepresentation,
 ): CordWithMoveType[] {
-    return [];
+    {
+        if (cord.y - moveDirection > 7 || cord.y - moveDirection < 0) {
+            return [];
+        }
+        //check is blocked
+        if (currentBoardState[cord.y - moveDirection * 2][cord.x] != null) {
+            return [];
+        }
+        const resultCordY = (cord.y - moveDirection * 2) as Cord['y'];
+        const result: CordWithMoveType[] = [{ x: 1, y: resultCordY, moveType: MoveType.NormalMove }];
+        return result;
+    }
 }
 export function getCapture(
     cord: Cord,
     moveDirection: number,
     currentBoardState: ChessBoardRepresentation,
 ): CordWithMoveType[] {
-    return [];
+    const result: CordWithMoveType[] = [];
+    const leftCorner = currentBoardState[cord.y - moveDirection][cord.x - 1];
+    const rightCorner = currentBoardState[cord.y - moveDirection][cord.x + 1];
+    const currentPawn = currentBoardState[cord.y][cord.x];
+    if (cord.y - moveDirection > 7 || cord.y - moveDirection < 0) {
+        return result;
+    }
+    if (leftCorner != null && leftCorner != undefined && leftCorner.side != currentPawn?.side) {
+        result.push({
+            x: leftCorner.cord.x,
+            y: leftCorner.cord.y,
+            moveType: MoveType.Capture,
+        });
+    }
+    if (rightCorner != null && rightCorner != undefined && rightCorner.side != currentPawn?.side) {
+        result.push({
+            x: rightCorner.cord.x,
+            y: rightCorner.cord.y,
+            moveType: MoveType.Capture,
+        });
+    }
+    return result;
 }
 export function getPassant(
     cord: Cord,
     moveDirection: number,
     currentBoardState: ChessBoardRepresentation,
+    previousBoardState: ChessBoardRepresentation,
 ): CordWithMoveType[] {
-    return [];
+    const result: CordWithMoveType[] = [];
+    const leftSide = currentBoardState[cord.y - moveDirection][cord.x - 1];
+    const rightSide = currentBoardState[cord.y - moveDirection][cord.x + 1];
+    const currentPawn = currentBoardState[cord.y][cord.x];
+
+    if (
+        previousBoardState[cord.y - moveDirection * 2][cord.x - 1]?.figType == PieceType.Pawn &&
+        previousBoardState[cord.y - moveDirection * 2][cord.x - 1]?.side != currentPawn?.side
+    ) {
+        if (rightSide != null && rightSide != undefined && rightSide.side != currentPawn?.side) {
+            result.push({
+                x: rightCorner.cord.x,
+                y: rightCorner.cord.y,
+                moveType: MoveType.Capture,
+            });
+        }
+    }
+
+    if (
+        previousBoardState[cord.y - moveDirection * 2][cord.x + 1]?.figType == PieceType.Pawn &&
+        previousBoardState[cord.y - moveDirection * 2][cord.x + 1]?.side != currentPawn?.side
+    ) {
+        if (leftSide != null && leftSide != undefined && leftSide.side != currentPawn?.side) {
+            result.push({
+                x: rightCorner.cord.x,
+                y: rightCorner.cord.y,
+                moveType: MoveType.Capture,
+            });
+        }
+    }
 }
 export function getPromotion(
     cord: Cord,
     moveDirection: number,
     currentBoardState: ChessBoardRepresentation,
 ): CordWithMoveType[] {
-    return [];
+    if (cord.y - moveDirection > 7 || cord.y - moveDirection < 0) {
+        return [];
+    }
+    //check is blocked
+    if (currentBoardState[cord.y - moveDirection][cord.x] != null) {
+        return [];
+    }
+    const resultCordY = (cord.y - moveDirection) as Cord['y'];
+    const result: CordWithMoveType[] = [{ x: 1, y: resultCordY, moveType: MoveType.Promotion }];
+    return result;
 }
 
 export function getPossibleMovesForPawn(
