2846515c8784750c4f51bb527d55a25bac8fa7ac
Ania-Em <aniaarchi@gmail.com>
2021-01-31 00:33:06 +0100

    Modules css fixed

---
+++ src/app/components/genericLabel/Label.module.scss.d.ts
@@ -0,0 +1,6 @@
+// This file is generated automatically
+export const labelwrapper: string;
+export const blue: string;
+export const red: string;
+export const yellow: string;
+export const labelline: string;
