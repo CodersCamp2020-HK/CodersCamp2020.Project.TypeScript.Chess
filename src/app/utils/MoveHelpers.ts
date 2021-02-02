ae48fda82a046bdbb8269ac77e2e34b3b6559062
Mateusz Król <37661247+KrolMateusz@users.noreply.github.com>
2021-02-02 17:34:17 +0100

    King, Rook, Bishop moves (#29)
    * Init
    
    * Lodash added, tsconfig updated
    
    * Working bishop moves, moves outside chessboard removed
    
    * Deleted moves outside board
    
    * Added function for removing cords outside board
    
    * Cords of other Pieces
    
    * Working bishop
    
    * Working bishop
    
    * Whole implementation in getPossibleMovesForBishop
    
    * Revert "Cords of other Pieces"
    
    This reverts commit 8b6f0550f72b0f923b27d20536b48d8dbd0ddd66.
    
    * Other pieces cords found
    
    * Checking blocked moves
    
    * Excluding moves behind Piece
    
    * Removing moves blocked by piece refactor
    
    * getPossibleMovesForBishop finished
    
    * King, Rook, Bishop working moves
    
    * King, Rook, Bishop, Queen
    
    * Refactor
    
    * Refactor for ChessEngine
    
    * Functions for getting possible moves for pieces moved to utils
    
    * Game component in App.ts
    
    * Deleted unnecessary files
    
    * Tests for moves for queen, bishop and rook
    
    * King tests added for all moves
    
    * Bishop tests when blocked
    
    * Chess moves test deleted. ChessEngine tests for rook added
    
    * ChessEngine tests for king added
    
    * ChessEngine tests for queen added
    
    * ChessEngine tests for bishop, queen, rook, king
    
    * Directions from MoveHelpers tested
    
    * Tests for removing moves outside board
    
    * Tests finished for MoveHelpers
    
    * Emoji board to chessboard representation
    
    * Emoji board to move types
    
    * Tests for chess engine with emojis written
    
    * MoveHelpers tests with emoji
    
    * Emoji board tested
    
    * Display.ts tests
    
    * Displaying emoji boards added

---
+++ src/app/utils/MoveHelpers.ts
@@ -0,0 +1,115 @@
+import { Cord, CordWithMoveType, IChessBoard, MoveType, PossibleCords, Side } from '../domain/basicChessTypes';
+import _ from 'lodash';
+
+export function getBishopDirections(cord: Cord): Cord[] {
+    const { x, y } = cord;
+    const directions = [
+        ..._.zip(_.range(x + 1, 8, 1), _.range(y + 1, 8, 1)),
+        ..._.zip(_.range(x + 1, 8, 1), _.range(y - 1, -1, -1)),
+        ..._.zip(_.range(x - 1, -1, -1), _.range(y + 1, 8, 1)),
+        ..._.zip(_.range(x - 1, -1, -1), _.range(y - 1, -1, -1)),
+    ];
+    const filteredDirections = directions.filter((cord) => cord[0] !== undefined && cord[1] !== undefined);
+    return filteredDirections.map((cord) => {
+        return { x: cord[0], y: cord[1] } as Cord;
+    });
+}
+
+export function getKingDirections(cord: Cord): PossibleCords[] {
+    const numbers = [-1, 1];
+    const possibleMoves = [];
+
+    for (const i of numbers) {
+        const actualKingPositionX = cord.x + i;
+        const actualKingPositionY = cord.y;
+        possibleMoves.push({ x: actualKingPositionX, y: actualKingPositionY });
+    }
+
+    for (const i of numbers) {
+        const actualKingPositionX = cord.x;
+        const actualKingPositionY = cord.y + i;
+        possibleMoves.push({ x: actualKingPositionX, y: actualKingPositionY });
+    }
+
+    for (const i of numbers) {
+        for (const j of numbers) {
+            const actualKingPositionX = cord.x + i;
+            const actualKingPositionY = cord.y + j;
+            possibleMoves.push({ x: actualKingPositionX, y: actualKingPositionY });
+        }
+    }
+
+    return possibleMoves;
+}
+
+export function getRookDirections(cord: Cord): Cord[] {
+    const arr: Cord[] = [];
+    for (let i = 0; i <= 7; i++) {
+        arr.push({ x: cord.x, y: i } as Cord);
+    }
+    for (let i = 0; i <= 7; i++) {
+        arr.push({ x: i, y: cord.y } as Cord);
+    }
+    const result = _.differenceWith(arr, [cord], _.isEqual);
+    return result;
+}
+
+const insideBoard = (v: number) => v >= 0 && v <= 7;
+export function removeMovesOutsideChessBoard(cords: PossibleCords[]): Cord[] {
+    return cords.filter(({ x, y }) => insideBoard(x as number) && insideBoard(y as number)) as Cord[];
+}
+
+export function removeMovesBlockedByPiece(
+    pieceCord: Cord,
+    possibleMovesCords: Cord[],
+    boardState: IChessBoard,
+): Cord[] {
+    const otherPiecesCords = getOtherPiecesCord(possibleMovesCords, boardState);
+    const result = excludeMovesBehindPiece(pieceCord, possibleMovesCords, otherPiecesCords);
+
+    return result;
+}
+
+export function getOtherPiecesCord(possibleMovesCords: Cord[], boardState: IChessBoard): Cord[] {
+    return possibleMovesCords.filter((cord) => {
+        const square = boardState.board[cord.x][cord.y];
+        return square ? true : false;
+    });
+}
+
+export function excludeMovesBehindPiece(pieceCord: Cord, possibleMovesCords: Cord[], otherPiecesCords: Cord[]): Cord[] {
+    const diffArr: Cord[] = [];
+
+    for (const otherPieceCord of otherPiecesCords) {
+        const vector = { x: otherPieceCord.x - pieceCord.x, y: otherPieceCord.y - pieceCord.y };
+        let versorX;
+        let versorY;
+        vector.x === 0 ? (versorX = 0) : vector.x > 0 ? (versorX = 1) : (versorX = -1);
+        vector.y === 0 ? (versorY = 0) : vector.y > 0 ? (versorY = 1) : (versorY = -1);
+        for (let i = 1; i <= 7; i++) {
+            const oneDiffCord = { x: otherPieceCord.x + versorX * i, y: otherPieceCord.y + versorY * i } as Cord;
+            if (oneDiffCord.x > 7 || oneDiffCord.x < 0 || oneDiffCord.y > 7 || oneDiffCord.y < 0) break;
+            diffArr.push(oneDiffCord);
+        }
+    }
+
+    return _.differenceWith(possibleMovesCords, diffArr, _.isEqual);
+}
+
+export function getMoveTypesForPiece(cords: Cord[], side: Side, boardState: IChessBoard): CordWithMoveType[] {
+    const result: CordWithMoveType[] = [];
+    for (const move of cords) {
+        const { x, y } = move;
+        const square = boardState.board[x][y];
+        if (square) {
+            const otherSide = square.side;
+            if (otherSide !== side) {
+                result.push({ x, y, moveType: MoveType.Capture });
+            }
+        } else {
+            result.push({ x, y, moveType: MoveType.NormalMove });
+        }
+    }
+
+    return result;
+}
