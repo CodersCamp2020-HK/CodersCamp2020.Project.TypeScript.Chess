cebb2637a303096eb147550e2168ca0acae6215e
Weronika Dziedzic <wera02@gmail.com>
2021-02-03 13:52:11 +0100

    Header add title and data

--- src/app/components/HeaderCyberChess/Header.ts
+++ src/app/components/HeaderCyberChess/Header.ts
@@ -6,6 +6,16 @@ export class Header {
     constructor() {
         this.__element = document.createElement('div');
         this.__element.classList.add(styles.header);
+
+        const cyberChess = document.createElement('div');
+        cyberChess.classList.add(styles.cyberChess);
+        cyberChess.textContent = 'Cyber Chess';
+
+        const cyberData = document.createElement('div');
+        cyberData.classList.add(styles.cyberChess);
+        cyberData.textContent = '2021';
+
+        this.__element.append(cyberChess, cyberData);
     }
 
     public get element(): HTMLElement {
