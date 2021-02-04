3c390aa5904d50349e5c3e397efd2336b175a944
Mateusz Król <matek8952@gmail.com>
2021-02-04 21:26:39 +0100

    Tests for getPiece and getPieces

--- spec/Chessboard.spec.ts
+++ spec/Chessboard.spec.ts
@@ -1,4 +1,4 @@
-import { Piece } from '../src/app/domain/basicChessTypes';
+import { Piece, PieceType, Side } from '../src/app/domain/basicChessTypes';
 import { ChessBoard } from '../src/app/infrastructure/ChessBoard';
 import { convertEmojiToRep, displayEmojiBoard } from './Display';
 
@@ -18,6 +18,31 @@ describe(`Given: Starting chessboard :`, () => {
         const expected = convertEmojiToRep(expectedEmoji);
         it(`Then: board should look like: \n${displayEmojiBoard(expectedEmoji)}`, () => {
             expect(chessboard.board).toEqual(expect.arrayContaining(expected));
+        });
+    });
+    describe(`When: getPiece( cord = { x: 0, y: 6 }) is invoked`, () => {
+        const expected: Piece = {
+            cord: { x: 0, y: 6 },
+            figType: PieceType.Knight,
+            side: Side.Black,
+            isMoved: false,
+        };
+        it(`Then: result should be \n${JSON.stringify(expected, null, 4)}`, () => {
+            const actual = chessboard.getPiece({ x: 0, y: 6 });
+            expect(actual).toEqual(expect.objectContaining(expected));
+        });
+    });
+    describe(`When: getPiece( cord = { x: 5, y: 1 }) is invoked`, () => {
+        it(`Then: result should be null`, () => {
+            const actual = chessboard.getPiece({ x: 5, y: 1 });
+            expect(actual).toBeNull;
+        });
+    });
+    describe(`When: getPieces( side = Side.Black, piece = PieceType.King) is invoked`, () => {
+        const expected: Piece[] = [{ cord: { x: 0, y: 4 }, figType: PieceType.King, isMoved: false, side: Side.Black }];
+        it(`Then: result should be \n${JSON.stringify(expected, null, 4)}`, () => {
+            const actual = chessboard.getPieces(Side.Black, PieceType.King);
+            expect(actual).toEqual(expect.arrayContaining(expected));
         });
     });
     describe(`When: white piece moved from { x: 6, y: 4 } to { x: 4, y: 4 }`, () => {
