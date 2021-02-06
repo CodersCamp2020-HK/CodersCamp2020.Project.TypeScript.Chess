29be2db09dd9faf11e0430ce2f884a0bb401c64e
Mateusz Król <matek8952@gmail.com>
2021-02-06 13:08:31 +0100

    Previous boards state

--- src/app/infrastructure/GameState.ts
+++ src/app/infrastructure/GameState.ts
@@ -1,15 +1,23 @@
-import { IChessBoard, Side } from '../domain/basicChessTypes';
+import { ChessBoardRepresentation, IChessBoard, Side } from '../domain/basicChessTypes';
 import { getCapturedPieceNames } from '../utils/CapturedPieces';
 
 export class GameState {
     private __capturedPieces: { white: string[]; black: string[] }[];
+    private __previousBoards: ChessBoardRepresentation[];
 
     constructor() {
         this.__capturedPieces = [];
+        this.__previousBoards = [];
     }
 
     public get capturedPieces(): { white: string[]; black: string[] }[] {
         return this.__capturedPieces;
+    }
+
+    getPreviousBoards(moveNumber: number): ChessBoardRepresentation {
+        if (moveNumber < 0 || moveNumber > this.__previousBoards.length - 1)
+            throw new Error(`There is no move with number: ${moveNumber} `);
+        return this.__previousBoards[moveNumber];
     }
 
     updateCapturedPieces(boardState: IChessBoard): void {
@@ -18,5 +26,9 @@ export class GameState {
 
         const capturedPiece = { black: whitePiecesNames, white: blackPieceNames };
         this.__capturedPieces.push(capturedPiece);
+    }
+
+    updatePreviousBoards(chessboard: IChessBoard): void {
+        this.__previousBoards.push(chessboard.board);
     }
 }
