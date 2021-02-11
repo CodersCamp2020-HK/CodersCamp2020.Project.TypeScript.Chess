e5a00ac666fe34bdccbefcc52c3cd3c5b9e19938
Haivex <demozial@gmail.com>
2021-02-11 15:15:30 +0100

    Refactor code

--- src/app/components/PreviousMoves/previousMoves.module.scss.d.ts
+++ src/app/components/PreviousMoves/previousMoves.module.scss.d.ts
@@ -1,5 +1,6 @@
 // This file is generated automatically
 export const listItem: string;
 export const wrapper: string;
+export const pieceImage: string;
 export const player: string;
 export const opponent: string;
--- src/app/components/PreviousMoves/previousMoves.ts
+++ src/app/components/PreviousMoves/previousMoves.ts
@@ -2,14 +2,12 @@ import styles from './previousMoves.module.scss';
 import { piecesArray } from '../PiecesElements/piecesElements';
 import { PieceElement } from '../ChessBoard/ChessBoardComponent';
 import { Side, PieceType } from '../../domain/basicChessTypes';
+import { opponent } from '../game/capturedTable/CapturedTable.module.scss';
 
 interface Notation {
     white: string;
     black: string;
-}
-
-interface Object {
-    [idx: string]: string;
+    [key: string]: string;
 }
 
 export class PreviousMoves {
@@ -32,60 +30,41 @@ export class PreviousMoves {
             li.classList.add(styles.listItem);
 
             const piecesMap = new Map([
-                ['K', 5],
-                ['Q', 1],
-                ['R', 4],
-                ['B', 3],
-                ['N', 2],
-                ['P', 0],
+                ['K', PieceType.King],
+                ['Q', PieceType.Queen],
+                ['R', PieceType.Rook],
+                ['B', PieceType.Bishop],
+                ['N', PieceType.Knight],
+                ['P', PieceType.Pawn],
             ]);
 
-            const currentTypeBlue = piecesMap.get(notation.black[0])!;
+            for (const color in notation) {
+                const sideColor = color == 'white' ? Side.White : Side.Black;
+                const currentClassName = color == 'white' ? 'player' : 'opponent';
 
-            const pieceElementBlue: PieceElement | undefined = piecesArray.find((element) => {
-                if (element.figType == currentTypeBlue && element.side == Side.Black) {
-                    return true;
-                }
-            });
+                const pieceAbbreviation: string = notation[color][0];
+                const currentFigType = piecesMap.get(pieceAbbreviation);
+                const pieceOrigin = notation[color].slice(1, 3);
+                const pieceDestination = notation[color].slice(3, 5);
 
-            const blueOrigin = notation.black.slice(1, 3);
-            const blueDestination = notation.black.includes('x')
-                ? notation.black.slice(4, 6)
-                : notation.black.slice(3, 5);
+                const pieceElement: PieceElement | undefined = piecesArray.filter((element) => {
+                    if (element.figType == currentFigType && element.side == sideColor) {
+                        return true;
+                    }
+                })[0];
 
+                const pieceImage = pieceElement.element.cloneNode(true) as HTMLImageElement;
+                pieceImage.classList.add(styles.pieceImage);
+
+                const span = document.createElement('span');
+                span.append(pieceImage, `${pieceOrigin} > ${pieceDestination} `);
+                span.classList.add(styles[currentClassName]);
+
+                li.append(span);
+            }
+
+            li.insertBefore(document.createTextNode(' | '), li.lastChild);
             //`${bluePiece} ${blueOrigin} > ${blueDestination} `
-
-            const imgBlue = pieceElementBlue?.element.cloneNode(true)! as HTMLImageElement;
-            imgBlue.width = 18;
-            imgBlue.height = 18;
-
-            const blueSpan = document.createElement('span');
-            blueSpan.append(imgBlue, `${blueOrigin} > ${blueDestination} `);
-            blueSpan.classList.add(styles.opponent);
-
-            const currentTypeRed = piecesMap.get(notation.white[0])!;
-
-            const pieceElementRed: PieceElement | undefined = piecesArray.find((element) => {
-                if (element.figType == currentTypeRed && element.side == Side.White) {
-                    return true;
-                }
-            });
-
-            const imgRed = pieceElementRed?.element.cloneNode(true)! as HTMLImageElement;
-            imgRed.width = 18;
-            imgRed.height = 18;
-
-            const redPiece = notation.white[0];
-            const redOrigin = notation.white.slice(1, 3);
-            const redDestination = notation.white.includes('x')
-                ? notation.white.slice(4, 6)
-                : notation.white.slice(3, 5);
-
-            const redSpan = document.createElement('span');
-            redSpan.append(imgRed, `${redOrigin} > ${redDestination} `);
-            redSpan.classList.add(styles.player);
-
-            li.append(blueSpan, document.createTextNode(' | '), redSpan);
 
             ol.append(li);
         });
