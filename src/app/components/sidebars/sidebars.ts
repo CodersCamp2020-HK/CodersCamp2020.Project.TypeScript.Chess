3c855d9704fbe6ce2e82228d4affe3879c5e82cf
Haivex <demozial@gmail.com>
2021-02-12 15:45:51 +0100

    Rules without images in actions

--- src/app/components/sidebars/sidebars.ts
+++ src/app/components/sidebars/sidebars.ts
@@ -1,4 +1,5 @@
-import { data } from './infoData.ts';
+import { data } from './infoData';
+import { data as rulesData } from './rulesData';
 import styles from './sidebars.module.scss';
 
 export class InfoContent {
@@ -13,18 +14,18 @@ export class InfoContent {
 
     render(): void {
         this._element.innerHTML = '';
-        this._element.classList.add(styles.mainWrapper);
+        this._element.classList.add(styles.mainWrapperInfo);
 
         const logo = document.createElement('span');
-        logo.textContent = data.logo;
+        logo.textContent = this._data.logo;
         logo.classList.add(styles.logo);
 
         const year = document.createElement('span');
-        year.textContent = data.year;
+        year.textContent = this._data.year;
         year.classList.add(styles.year);
 
         const description = document.createElement('p');
-        description.textContent = data.description;
+        description.textContent = this._data.description;
         description.classList.add(styles.description);
 
         const mentorWrapper = document.createElement('div');
@@ -32,7 +33,7 @@ export class InfoContent {
         mentorLabel.textContent = 'Mentor';
         mentorLabel.classList.add(styles.label);
         const mentorName = document.createElement('span');
-        mentorName.textContent = data.mentor;
+        mentorName.textContent = this._data.mentor;
         mentorWrapper.classList.add(styles.mentor);
         mentorWrapper.append(mentorLabel, mentorName);
 
@@ -42,7 +43,7 @@ export class InfoContent {
         teamLabel.classList.add(styles.label);
         const teamList = document.createElement('ol');
         teamList.classList.add(styles.teamList);
-        data.team.forEach((name) => {
+        this._data.team.forEach((name) => {
             const li = document.createElement('li');
             li.textContent = name;
             teamList.append(li);
@@ -55,8 +56,8 @@ export class InfoContent {
         linkLabel.textContent = 'Link to github';
         linkLabel.classList.add(styles.label);
         const link = document.createElement('a');
-        link.textContent = data.link;
-        link.href = data.link;
+        link.textContent = this._data.link;
+        link.href = this._data.link;
         link.target = '_blank';
         link.classList.add(styles.link);
         linkWrapper.classList.add(styles.linkWrapper);
@@ -68,31 +69,77 @@ export class InfoContent {
 
 export class RulesContent {
     private _element: HTMLDivElement = document.createElement('div');
+    private _data = rulesData;
     constructor() {
         this.render();
     }
 
     render(): void {
         this._element.innerHTML = '';
-        this._element.classList.add(styles.mainWrapper);
+        this._element.classList.add(styles.mainWrapperRules);
 
         const piecesLabel = document.createElement('label');
         piecesLabel.textContent = 'Chess moves';
         piecesLabel.classList.add(styles.rulesLabel);
 
+        const piecesContainer = document.createElement('div');
+        piecesContainer.classList.add(styles.piecesContainer);
+
+        this._data.pieces.forEach((piece) => {
+            const pieceCard = document.createElement('div');
+            pieceCard.classList.add(styles.pieceCard);
+
+            const pieceContainerImage = document.createElement('div');
+            pieceContainerImage.classList.add(styles.pieceContainerImage);
+            const pieceImage = piece.img.cloneNode(true) as HTMLImageElement;
+            pieceImage.classList.add(styles.pieceImage);
+            pieceContainerImage.append(pieceImage);
+
+            const pieceDescription = document.createElement('div');
+            pieceDescription.classList.add(styles.pieceDescription);
+
+            const pieceName = document.createElement('span');
+            pieceName.classList.add(styles.pieceName);
+            pieceName.textContent = piece.name;
+
+            const pieceText = document.createElement('p');
+            pieceText.classList.add(styles.pieceText);
+            pieceText.textContent = piece.description;
+
+            pieceDescription.append(pieceName, pieceText);
+
+            pieceCard.append(pieceContainerImage, pieceDescription);
+
+            piecesContainer.append(pieceCard);
+        });
         const castlingLabel = document.createElement('label');
         castlingLabel.textContent = 'Castling';
         castlingLabel.classList.add(styles.rulesLabel);
+
+        const actionsContainer = document.createElement('div');
+        actionsContainer.classList.add(styles.actionsContainer);
+
+        this._data.actions.forEach((action) => {
+            const actionCard = document.createElement('div');
+            actionCard.classList.add(styles.actionCard);
+
+            const actionName = document.createElement('span');
+            actionName.textContent = action.name;
+            actionName.classList.add(styles.actionName);
+
+            const actionDescription = document.createElement('p');
+            actionDescription.textContent = action.description;
+            actionDescription.classList.add(styles.actionDescription);
+
+            actionCard.append(actionName, actionDescription);
+            actionsContainer.append(actionCard);
+        });
 
         const enPassantLabel = document.createElement('label');
         enPassantLabel.textContent = 'enPassant';
         enPassantLabel.classList.add(styles.rulesLabel);
 
-        const pawnPromotionLabel = document.createElement('label');
-        pawnPromotionLabel.textContent = 'enPassant';
-        pawnPromotionLabel.classList.add(styles.rulesLabel);
-
-        this._element.append(piecesLabel);
+        this._element.append(piecesLabel, piecesContainer, actionsContainer);
     }
     get element(): HTMLDivElement {
         return this._element;
