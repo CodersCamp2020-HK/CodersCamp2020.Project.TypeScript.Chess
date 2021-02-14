fa3d08f113131856ee394b379ee192144b9d4981
Ania-Em <74919839+Ania-Em@users.noreply.github.com>
2021-02-14 14:43:27 +0100

    Modal Quit Button (#100)
    * Modal with buttons
    
    * Bug
    
    Co-authored-by: bat098 <75204583+bat098@users.noreply.github.com>

--- src/app/components/PreviousMoves/previousMoves.module.scss.d.ts
+++ src/app/components/PreviousMoves/previousMoves.module.scss.d.ts
@@ -1,5 +1,7 @@
+
 export const listItem: string;
 export const opponent: string;
 export const pieceImage: string;
 export const player: string;
 export const wrapper: string;
+
--- src/app/components/genericLabel/Label.module.scss.d.ts
+++ src/app/components/genericLabel/Label.module.scss.d.ts
@@ -1,6 +1,5 @@
-// This file is generated automatically
-export const wrapper: string;
 export const blue: string;
-export const red: string;
-export const yellow: string;
 export const line: string;
+export const red: string;
+export const wrapper: string;
+export const yellow: string;
