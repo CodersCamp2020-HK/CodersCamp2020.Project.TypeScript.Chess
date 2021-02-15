235d595995b0a168ec6f7342e862ddff47563379
Ania-Em <aniaarchi@gmail.com>
2021-02-15 23:34:55 +0100

    bug

--- src/app/components/MainMenu/MainMenu.module.scss.d.ts
+++ src/app/components/MainMenu/MainMenu.module.scss.d.ts
@@ -5,3 +5,8 @@ export const mainMenuSettingsWrapper: string;
 export const wrapper: string;
 export const wrapperText: string;
 export const wrapperFooter: string;
+export const playWithDiv: string;
+export const timePerPlayerDiv: string;
+export const enterYourNameDiv: string;
+export const glitched: string;
+export const glitchedChessboard: string;
