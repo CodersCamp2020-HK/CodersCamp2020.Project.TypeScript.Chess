f6b20567655f6326112f2b2c2dd860d5597ff74e
Mateusz Król <matek8952@gmail.com>
2021-02-15 23:10:40 +0100

    Type files modified

--- src/app/components/Input/input.module.scss.d.ts
+++ src/app/components/Input/input.module.scss.d.ts
@@ -1,6 +1,7 @@
-export const errors: string;
+// This file is generated automatically
 export const wrapper: string;
 export const input: string;
 export const wrong: string;
 export const errors: string;
-
+export const glitched: string;
+export const glitchedChessboard: string;
--- src/app/components/radios/radios.module.scss.d.ts
+++ src/app/components/radios/radios.module.scss.d.ts
@@ -1,7 +1,7 @@
-export const glitched: string;
-export const glitchedChessboard: string;
+// This file is generated automatically
 export const label: string;
 export const radio: string;
 export const radioGroup: string;
 export const option: string;
-
+export const glitched: string;
+export const glitchedChessboard: string;
