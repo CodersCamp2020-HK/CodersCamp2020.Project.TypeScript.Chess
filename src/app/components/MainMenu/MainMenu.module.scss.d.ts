ac34d7ecfdbb8f31d829fff30589c967f9ff8c95
Ania-Em <aniaarchi@gmail.com>
2021-02-15 22:14:17 +0100

    merge

---
+++ src/app/components/MainMenu/MainMenu.module.scss.d.ts
@@ -0,0 +1,9 @@
+// This file is generated automatically
+export const container: string;
+export const startTheGameButton: string;
+export const mainMenuSettingsWrapper: string;
+export const wrapper: string;
+export const wrapperText: string;
+export const wrapperFooter: string;
+export const glitched: string;
+export const glitchedChessboard: string;
