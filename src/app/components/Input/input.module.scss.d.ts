069ae3cc99897a5c418fb796f27f041c69a3e555
Tomasz Dudek <t.dudeek@gmail.com>
2021-02-16 16:39:31 +0100

    Updated MainMenu

--- src/app/components/Input/input.module.scss.d.ts
+++ src/app/components/Input/input.module.scss.d.ts
@@ -1,8 +1,4 @@
 // This file is generated automatically
-<<<<<<< HEAD
-export const wrapper: string;
-=======
->>>>>>> 21080fd758cf6c79dc3f3a296071f9553c5a9803
 export const input: string;
 export const wrapper: string;
 export const wrong: string;
--- src/app/components/MainMenu/MainMenu.module.scss.d.ts
+++ src/app/components/MainMenu/MainMenu.module.scss.d.ts
@@ -5,11 +5,9 @@ export const mainMenuSettingsWrapper: string;
 export const wrapper: string;
 export const wrapperText: string;
 export const wrapperFooter: string;
-<<<<<<< HEAD
-=======
 export const playWithDiv: string;
 export const timePerPlayerDiv: string;
+export const levelDiv: string;
 export const enterYourNameDiv: string;
->>>>>>> 21080fd758cf6c79dc3f3a296071f9553c5a9803
 export const glitched: string;
 export const glitchedChessboard: string;
